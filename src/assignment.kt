7df4d08e4e94a7fd2a359f2d73022bdfd9e38b80
intwaza <iintwazabelyse@gmail.com>
2021-02-20 21:46:15 +0300

    first assignment

---
+++ src/assignment.kt
@@ -0,0 +1,13 @@
+fun main() {
+    var student= "Belyse"
+    println(student)
+    var age= 20
+    println(age)
+    var phonenumber= "0781877792"
+    println(phonenumber)
+    var weight= 58
+    println(weight)
+    var citizenship="kenya"
+    var Kenya: Boolean= true
+    println(Kenya)
+}
